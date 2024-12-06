973b33b0d24ebf878840ab15ed7dc9749c3331c2
Franco Henker <francohenker@gmail.com>
2024-12-06 14:53:58 -0300

    modificaciones carrito

--- src/main/java/unam/edu/ecomarket/repositorios/DetalleOrdenRepositorio.java
+++ src/main/java/unam/edu/ecomarket/repositorios/DetalleOrdenRepositorio.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import unam.edu.ecomarket.modelo.DetalleOrden;
+import unam.edu.ecomarket.modelo.Usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,6 +18,7 @@ public interface DetalleOrdenRepositorio extends JpaRepository<DetalleOrden, Int
 
     void removeById(Integer id);
 
+//    List<DetalleOrden> findByUser(Usuario usuario);
 
     @Modifying
     @Transactional
--- src/main/java/unam/edu/ecomarket/repositorios/OrdenRepositorio.java
+++ src/main/java/unam/edu/ecomarket/repositorios/OrdenRepositorio.java
@@ -1,9 +1,12 @@
 package unam.edu.ecomarket.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import unam.edu.ecomarket.modelo.Orden;
+import unam.edu.ecomarket.modelo.Usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +16,8 @@ public interface OrdenRepositorio extends JpaRepository<Orden, Integer> {
 
     void removeById(Integer id);
 
+    List<Orden> findByUsuario(Usuario usuario);
+
+    @Query("SELECT o FROM Orden o WHERE o.usuario = :usuario AND o.tipoPago = 'Seleccionar'")
+    List<Orden> findByUsuarioN(Usuario usuario);
 }
